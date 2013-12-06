13f2d79a7c9c68f1d7355589ad30e797ba16781a
dieg0o <aegerterdiego@gmail.com>
2013-12-06 16:52:05 +0100

    Added the Xtra Credits for Ex.No.5

--- ex5.rb
+++ ex5.rb
@@ -24,3 +24,37 @@ puts "His teeth are usually %s depending on the coffee and the f*cking cigarrett
 #this line is tricky, try to get it exactly right!!!
 
 puts "If I add %d %d, and %d I get %d." % [my_age, my_heigth, my_weight, my_age + my_heigth + my_weight]
+
+
+puts""
+puts"Extra Credit"
+puts"------------"
+puts""
+
+# 1.variables without 'my_'
+name = 'Diego E. Aegerter again!'
+age = 23 #true!
+heigth = 173 #cm
+weight = 63 #kg
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+# 2. more format sequences
+dicksize = 'XXL' #also true;P
+
+
+
+puts "Let's talk about %s." % name
+puts "He's %d cm tall." % heigth
+puts "He's %d pounds heavy." % weight
+puts "Actually that's not too heavy."
+puts "He's got %s eyes and %s hair." % [eyes, hair]
+puts "His teeth are usually %s depending on the coffee and the f*cking cigarrettes." % teeth
+puts "If I add %d %d, and %d I get %d." % [age, heigth, weight, age + heigth + weight]
+puts "And he has a really %s nose.p" % [dicksize]
+
+
+#Not finished yet, can't convert inces to centimetres / pounds to kilogramms.
+
+
+
