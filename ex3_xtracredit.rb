02e677f5cb153ead10006721f777f2d0f7a9b6f3
dieg0o <aegerterdiego@gmail.com>
2013-12-06 14:21:04 +0100

    first commit

---
+++ ex3_xtracredit.rb
@@ -0,0 +1,20 @@
+puts "Exercise No.3 - Numbers and Math - EXTRA CREDIT"
+puts "-------------------------------------"
+puts ""
+puts "Hard calculation:"
+puts"------------------"
+puts""
+puts"Last month, I became a lot of speedtickets at my car. -Let's calculate how many taxes I paid extra for my lovely County"
+puts "I driven on the Highway with a speed of 147 km/h -  Allowed were 120 km/h"
+puts "So I drive more than ", 147 - 120, "too fast!"
+
+puts""
+
+puts"If we now multiply 27 km/h with 40$ per Kilometre i driven to fast, we get:", 27 * 40,"CHF SwissFrancs for a speedticket!" 
+puts"Have fun!"
+puts""
+puts"Exchange rate Dollar - Swissfranc"
+
+puts"One US-Dollar are" 
+puts 1*0.91 
+puts"CHF SwissFrancs"
---
+++ ex4.rb
@@ -0,0 +1,3 @@
+puts "Exercise No.4 - Variables And Names"
+puts "-------------------------------------"
+puts ""
