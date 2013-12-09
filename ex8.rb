669eb470b583500641d1ce4db4cde79460caecf1
dieg0o <aegerterdiego@gmail.com>
2013-12-09 15:08:23 +0100

    Added Ex8 with ExtraCredit in comments.

---
+++ ex8.rb
@@ -0,0 +1,23 @@
+puts ""
+puts "Exercise No.8 - Printing, Printing"
+puts "-------------------------------------"
+puts ""
+
+formatter = "%s %s %s %s "
+
+puts formatter % [1, 2, 3, 4]
+puts formatter % ["one", "two", "three", "four"]
+puts formatter % [true, false, false, true]
+puts formatter % [formatter, formatter, formatter, formatter]
+puts formatter % [
+									"I had this thing.",
+									"That you colud type up right.",
+									"But it didn't sing.",
+									"So I sad goodnight."
+									]
+
+
+#ExtraCredit - My mistakes
+#
+#Kommafehler bide letzte puts mit Absatz zwüsche de " ", hanni aber grad bemerkt.
+#Tippfehler vo einm "formatter" woni überseh ha. 
